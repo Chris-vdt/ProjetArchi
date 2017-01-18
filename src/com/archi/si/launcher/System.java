3f9b437d809961f2b82a698da3b67590f249787b
Lea <lea.dam@live.fr>
2017-01-18 15:41:02 +0100

    System et Display

--- src/com/archi/si/launcher/System.java
+++ src/com/archi/si/launcher/System.java
@@ -1,10 +1,19 @@
 package com.archi.si.launcher;
 
+
+import com.archi.si.view.Display;
+
 public class System {
 
-	public static void main(String[] args) {
+	static Display dis = new Display();
+	private static Object out;
+	
+	
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
 
+		System.out.println(dis.afficherMenu());
+		
 	}
 
 }
---
+++ src/com/archi/si/view/Display.java
@@ -0,0 +1,18 @@
+package com.archi.si.view;
+
+public class Display {
+
+	public String afficherMenu(){
+		return "1 - Afficher les commandes"
+				+ "\r\n2 - Ajouter une commande"
+				+ "\r\n3 - Modifier une commande"
+				+ "\r\n4 - Afficher les OF"
+				+ "\r\n2 - Ajouter un OF"
+				+ "\r\n3 - Modifier un OF"
+				+ "\r\n99 - Quitter";
+				
+				
+	}
+	
+	
+}
